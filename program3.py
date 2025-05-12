46cda34057a0d701bcb2eb193355f1869fc48b95
suthirkumar <suthirkumar5151@gmail.com>
2025-05-12 15:57:38 +0530

    jgh

--- program3.py
+++
@@ -1,9 +0,0 @@
-# Calculate the area of a circle
-
-def_calculate_area(radius):
-    area=3.14159*radius*radius
-    return area
-
-rad=int(input("Enter the radius:"))
-print("The area of the circle is:",calculate_area(radius))
-
